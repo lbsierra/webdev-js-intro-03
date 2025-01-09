e7384ca5c0ba24e78f927a47c98bb6e4844d2415
Sierra <sonicaradia@gmail.com>
2025-01-09 08:14:28 -0500

    Did experimenting

--- js/index.js
+++ js/index.js
@@ -2,3 +2,36 @@
 // that are not declared
 "use strict"
 
+console.log(8 + 8);
+console.log(15 || 50);
+console.log(7 % 3);
+console.log(6 - 16);
+console.log(88 & 88);
+console.log(88 && 88);
+
+let a = 100;
+let b = 450;
+console.log(a << b);
+
+console.log(10 === "10");
+console.log(50 + "50");
+
+let c = 8;
+console.log(c += 8);
+
+let d = 44;
+console.log(d &= 5);
+
+let e = 7;
+console.log(~e);
+
+console.log(4 ** 4);
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
